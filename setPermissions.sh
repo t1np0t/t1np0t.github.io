9543e32a31e16c8de7bf12dc9d1cddf4515b5508
Darrion Nguyen <darrion0920@gmail.com>
2023-10-17 15:49:17 -0500

    10/17/23

---
+++ setPermissions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Allow this and descendant directories to be traversed by everyone, ignoring hidden directories
+
+find -L "$PWD" -not -path "*/.*" -type d -exec chmod a+X {} \;
+
+# Allow everyone read access to all files in this hierarchy with an extension of
+# ..html, .png, or .jng
+find -L "$PWD" -not -path "*/.*" -type f \( -name "*.html" -o -name "*.png" -o -name "*.jpg" \) -exec chmod a+r {} \;
