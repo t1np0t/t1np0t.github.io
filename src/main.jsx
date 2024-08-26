6f86409abc82c639b5d249a02a104e5781664e79
Darrion Nguyen <darrion0920@gmail.com>
2024-08-25 19:44:54 -0500

    fixed reloading error on website

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -44,7 +45,7 @@ export default function App() {
 }
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
