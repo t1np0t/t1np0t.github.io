568c17ea59c89d98b75e2dc8455485b387b09a2c
Darrion Nguyen <darrion0920@gmail.com>
2024-10-12 01:01:32 -0500

    got graphs working on article

--- src/components/graphs/TerrariumGraph.jsx
+++ src/components/graphs/TerrariumGraph.jsx
@@ -1,21 +1,65 @@
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis, YAxis } from "recharts"
+import { ChartContainer } from "@/components/ui/chart"
 
-export default function TerrariumGraph() {
-  const data = [
-    { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
-    { name: 'Page B', uv: 300, pv: 1398, amt: 2210 },
-    { name: 'Page C', uv: 200, pv: 9800, amt: 2290 },
-    { name: 'Page D', uv: 500, pv: 3908, amt: 2000 },
-    { name: 'Page E', uv: 600, pv: 4800, amt: 2181 },
-  ];
+const chartData = [
+  { test: "A", before: 3, after: 4 },
+  { test: "B", before: 6, after: 2 },
+  { test: "C", before: 4, after: 5 },
+]
 
-  return (
-      <BarChart width={600} height={300} data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Bar dataKey="uv" fill="#8884d8" />
-      </BarChart>
-  );
+const chartConfig = {
+  desktop: {
+    label: "Test",
+    color: "hsl(var(--chart-1))",
+  },
+  label: {
+    color: "hsl(var(--background))",
+  },
 };
+
+export default function HomePageGraph() {
+  return (
+    <ChartContainer config={chartConfig} className="w-[70vw] lg:w-[35vw]">
+      <BarChart data={chartData} layout="vertical" margin={{ right: 25 }}>
+        <CartesianGrid strokeDasharray={'5 5'} />
+        <YAxis
+          dataKey="test"
+          type="category"
+          tickLine={false}
+          tickMargin={10}
+          axisLine={false}
+          tickFormatter={(value) => value.slice(0, 3)}
+          hide
+        />
+        <XAxis dataKey="before" type="number" hide />
+        <Bar
+          dataKey="before"
+          layout="vertical"
+          fill="var(--color-desktop)"
+          radius={4}
+        >
+          <LabelList
+            dataKey="test"
+            position="insideLeft"
+            offset={8}
+            className="fill-[--color-label]"
+            fontSize={20}
+          />
+          <LabelList
+            dataKey="before"
+            position="right"
+            offset={8}
+            className="fill-background"
+            fontSize={20}
+          />
+        </Bar>
+        <Bar 
+          dataKey='after'
+          fill='var(--color-desktop)'
+          layout='vertical'
+          radius={4}
+        />
+      </BarChart>
+    </ChartContainer>
+  );
+}
